f92f277b742196ba481aba0417c5cb33e3493cf3
BPAdiv <adiv.balas@gmail.com>
2023-03-22 14:43:47 +0200

    sd

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -38,16 +38,42 @@ const product: amazonProduct = {
 };
 
 async function getProducts() {
-  const { data } = await axios.get(
-    "https://www.amazon.com/-/he/MacBook-%D7%96%D7%99%D7%9B%D7%A8%D7%95%D7%9F-%D7%90%D7%97%D7%95%D7%A8%D7%99%D7%AA-FaceTime-%D7%90%D7%99%D7%99%D7%A4%D7%95%D7%9F/dp/B08N5LNQCX/ref=sr_1_4?qid=1679261541&s=computers-intl-ship&sr=1-4&language=en_US&currency=USD"
-  );
-  const $ = cheerio.load(data);
-  product.link =
-    "https://www.amazon.com/-/he/MacBook-%D7%96%D7%99%D7%9B%D7%A8%D7%95%D7%9F-%D7%90%D7%97%D7%95%D7%A8%D7%99%D7%AA-FaceTime-%D7%90%D7%99%D7%99%D7%A4%D7%95%D7%9F/dp/B08N5LNQCX/ref=sr_1_4?qid=1679261541&s=computers-intl-ship&sr=1-4&language=en_US&currency=USD";
-  product.price = $(
-    " span.a-price.aok-align-center.reinventPricePriceToPayMargin.priceToPay > span:nth-child(2) > span.a-price-whole"
-  ).text();
-  product.name = $("#productTitle").text();
-  console.log(product);
+  try {
+    const { data } = await axios.get(
+      "https://www.amazon.com/gp/product/ajax?asin=B097T276QL&experienceId=aodAjaxMain",
+      {
+        headers: {
+          "user-agent":
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36",
+          "sec-ch-ua-platform": "Windows",
+          pragma: "no-cache",
+          accept: "text/html,*/*",
+          "x-requested-with": "XMLHttpRequest",
+          host: "www.amazon.com",
+          // cookie: ``,
+
+          // sec-ch-ua: "Google Chrome";v="111", "Not(A:Brand";v="8", "Chromium";v="111"
+        },
+      }
+    );
+    const $ = cheerio.load(data);
+    product.link =
+      "https://www.amazon.com/dp/B09TZWLFLY/ref=fs_a_mbt2_us2/132-5944599-9818649?_encoding=UTF8&th=1";
+    product.price = $("#aod-price-0  .a-offscreen").text();
+    product.name = $("#aod-asin-title-text").text();
+    console.log(product);
+  } catch (error) {
+    console.log(error);
+  }
+}
+function get() {
+  axios
+    .get(
+      "https://www.amazon.com/gp/product/ajax?asin=B09YCWFKNC&experienceId=aodAjaxMain"
+    )
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
 }
 getProducts();
+
+// https://www.amazon.com/gp/aod/ajax/?asin=B097T276QL&m=&smid=&sourcecustomerorglistid=&sourcecustomerorglistitemid=&sr=8-5&pc=dp
