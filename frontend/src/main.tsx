98c7476beddec0c1e5da3d96e76788c51fda162c
o6mer <o6merliraz@gmail.com>
2023-03-22 15:34:07 +0200

    frontend setup

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./home/HomePage";
+import "./index.css";
+import ErrorPage from "./general/ErrorPage";
+import ProductPage from "./product/ProductPage";
+import UserPage from "./user/UserPage";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/product/:productId",
+    element: <ProductPage />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/user/:userId",
+    element: <UserPage />,
+    errorElement: <ErrorPage />,
+  },
+]);
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
