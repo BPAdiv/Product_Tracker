7f15f246766f1fcd92e23ab73ad38b7a61f23842
BPAdiv <adiv.balas@gmail.com>
2023-05-07 18:13:01 +0300

    google alt homepage test

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
@@ -57,6 +57,7 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />,
   },
 ]);
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <UserProvider>
